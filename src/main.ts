6abffe5fe356fcc2569482ef04de6f375a71502d
clemersoncaetano <clemersoncaetano346@gmail.com>
2026-02-11 20:33:43 -0300

    continucao

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/Index'
-import './assets/styles/index.css'
-
-createApp(App)
-    .use(router)
-    .mount('#app')
